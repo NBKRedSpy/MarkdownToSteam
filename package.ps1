680e8a52a5e80d698c51d7425edc79751a707070
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2023-07-30 16:32:24 -0400

    Fixed code trimming issues

--- package.ps1
+++ package.ps1
@@ -7,11 +7,14 @@ md -Force -ErrorAction SilentlyContinue ./publish/packages/
 function publishOs($target)
 {
     Write-Output ("----- Building " + $target)
-    dotnet publish /p:DebugType=None /p:DebugSymbols=false --configuration Release --runtime $target --sc -o ./publish/targets/$target /p:PublishSingleFile=true .\MarkdownToSteam\MarkdownToSteam.csproj
+    dotnet publish /p:TrimeMode=partial /p:DebugType=None /p:DebugSymbols=false --configuration Release --runtime $target --sc -o ./publish/targets/$target /p:PublishTrimmed=true /p:PublishSingleFile=true .\MarkdownToSteam\MarkdownToSteam.csproj
     Compress-Archive -Force -DestinationPath ("./publish/packages/" + "MarkdownToSteam-" + $target + ".zip") ./publish/targets/$target/*
 }
 
 publishOs "osx-arm64"
 publishOs "win-x64"
 
-# dir -Directory .\publish\targets | %{Compress-Archive -Force -DestinationPath ("./publish/packages/" + $_.Name + ".zip") $_/* }    
+"------------  Files"
+
+dir -Recurse *.zip | %{write-host ($_.FullName + " ") -NoNewline }
+Write-Host
